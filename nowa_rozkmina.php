9e55041c233fed9bfc2a9306fa90f2f227556e67
HolyGeek404 <wiktorzme@gmail.com>
2019-04-18 16:00:26 +0200

    prawie skończona stylizacja

--- nowa_rozkmina.php
+++ nowa_rozkmina.php
@@ -4,14 +4,14 @@
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="add_thing.css">
 	<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
-	<link href="https://fonts.googleapis.com/css?family=Roboto:900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
 	<title>Rozkmina.pl - tu gdzie tworzy się myśl</title>
 </head>
 <body>
 <label>
 	<form action="insert.php" method="post">			
-		<input type="text" name="temat">
-		<textarea wrap="easy" name="tresc"></textarea>
+		<input type="text" name="temat" placeholder="Temat dla rozkminy">
+		<textarea wrap="easy" name="tresc" placeholder="Napisz, o czym myślisz . . ."></textarea>
 		<input type="submit">
 	</form>
 </label>
